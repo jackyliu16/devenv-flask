faa24dce24d3de4c8916a418fdbffedebf7ca88d
jackyliu16 <18922251299@163.com>
2023-06-13 00:49:50 +0800

    [SQL]: add data

---
+++ SQL/travel.sql
@@ -0,0 +1,21 @@
+CREATE TABLE user_data (
+    id  INT                 PRIMARY KEY AUTO_INCREMENT,
+
+    name    VARCHAR(36)     NOT NULL,
+    pwd     VARCHAR(36)     NOT NULL,
+
+    age         INT,
+    email       VARCHAR(36),
+    gender ENUM("female", "male", "unknown") NOT NULL DEFAULT "unknown"
+);
+
+source ./SQL/user.sql;
+
+CREATE TABLE admin (
+    id      INT             PRIMARY KEY AUTO_INCREMENT,
+    name    VARCHAR(36)     NOT NULL,
+    pwd     VARCHAR(36)     NOT NULL,
+    email   VARCHAR(36)
+);
+
+INSERT INTO admin VALUES (1000, "jacky", "123456", NULL);
