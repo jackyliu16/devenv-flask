c754426a4dacfa45c76750ccf21841895cbc9473
jackyliu16 <18922251299@163.com>
2023-06-14 08:49:45 +0800

    (db): Switch to encrypted password mode

--- SQL/travel.sql
+++ SQL/travel.sql
@@ -2,7 +2,7 @@ CREATE TABLE user (
     id  INT                 PRIMARY KEY AUTO_INCREMENT,
 
     name    VARCHAR(36)     NOT NULL,
-    pwd     VARCHAR(36)     NOT NULL,
+    pwd     CHAR(88)     NOT NULL,
 
     age         INT,
     email       VARCHAR(36),
@@ -15,7 +15,7 @@ source ./SQL/data/user.sql;
 CREATE TABLE admin (
     id      INT             PRIMARY KEY AUTO_INCREMENT,
     name    VARCHAR(36)     NOT NULL,
-    pwd     VARCHAR(36)     NOT NULL,
+    pwd     CHAR(88)     NOT NULL,
     email   VARCHAR(36)
 );
 
--- models.py
+++ models.py
@@ -7,7 +7,7 @@ class Admin(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(36), nullable=False)
-    pwd = db.Column(db.String(36), nullable=False)
+    pwd = db.Column(db.String(88), nullable=False)
     email = db.Column(db.String(36))
 
 class User(db.Model):
@@ -15,7 +15,7 @@ class User(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(36), nullable=False)
-    pwd = db.Column(db.String(36), nullable=False)
+    pwd = db.Column(db.String(88), nullable=False)
     age = db.Column(db.Integer)
     email = db.Column(db.String(36))
     gender = db.Column(db.Integer, server_default=db.FetchedValue())
