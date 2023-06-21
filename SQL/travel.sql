9827b1cce7acfaf50d54ef4ae0c0ff83a1eaa422
jackyliu16 <18922251299@163.com>
2023-06-22 02:57:47 +0800

    style(SQL): some format

--- SQL/travel.sql
+++ SQL/travel.sql
@@ -1,13 +1,14 @@
 CREATE TABLE user (
-    id  INT                 PRIMARY KEY AUTO_INCREMENT,
+    id          INT             PRIMARY KEY AUTO_INCREMENT,
 
-    name    VARCHAR(36)     NOT NULL,
-    pwd     CHAR(88)     NOT NULL,
+    name        VARCHAR(36)     NOT NULL,
+    pwd         CHAR(88)        NOT NULL,
 
     age         INT,
     email       VARCHAR(36),
     -- gender ENUM("female", "male", "unknown") NOT NULL DEFAULT "unknown"
-    gender INT  DEFAULT 2
+    gender      INT             DEFAULT 2
+    -- authority   BOOLEAN         
 );
 
 source ./SQL/data/user.sql;
@@ -15,7 +16,7 @@ source ./SQL/data/user.sql;
 CREATE TABLE admin (
     id      INT             PRIMARY KEY AUTO_INCREMENT,
     name    VARCHAR(36)     NOT NULL,
-    pwd     CHAR(88)     NOT NULL,
+    pwd     CHAR(88)        NOT NULL,
     email   VARCHAR(36)
 );
 
