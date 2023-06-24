71d1c9ee8b8a376733327a0ec6b24a1f5db88bdb
jackyliu16 <18922251299@163.com>
2023-06-24 22:31:58 +0800

    feat(db): implement feedback table and delete admin table file

--- SQL/data/user.sql
+++ SQL/data/user.sql
@@ -9,6 +9,7 @@
 --     gender ENUM("female", "male", "unknown") NOT NULL DEFAULT "unknown"
 -- );
                         --  id  fir last    name        pwd                                                                                      age   email        gender 
+INSERT INTO customer VALUES (1, "", "Anonymous", "Anonymous", "sha256$AH62hcfABzGv07dX$cc410d510852a5f49bd6bb544c7b0305e1c371c9dc4a494fa5912c089be9137a", 20, "Anonymous@god.com", 2);
 INSERT INTO customer VALUES (2, "", "jacky", "jacky1", "sha256$AH62hcfABzGv07dX$cc410d510852a5f49bd6bb544c7b0305e1c371c9dc4a494fa5912c089be9137a", 20, "1@189.com", 2);
 INSERT INTO customer VALUES (3, "", "jacky", "jacky2", "sha256$AH62hcfABzGv07dX$cc410d510852a5f49bd6bb544c7b0305e1c371c9dc4a494fa5912c089be9137a", 20, "2@189.com", 2);
 INSERT INTO customer VALUES (4, "", "jacky", "jacky3", "sha256$AH62hcfABzGv07dX$cc410d510852a5f49bd6bb544c7b0305e1c371c9dc4a494fa5912c089be9137a", 20, "3@189.com", 2);
--- SQL/travel.sql
+++ SQL/travel.sql
@@ -19,9 +19,16 @@ CREATE TABLE admin (
     pwd     CHAR(88)        NOT NULL,
     email   VARCHAR(36)     NOT NULL
 );
-
-source ./SQL/data/admin.sql;
+INSERT INTO admin VALUES (1, "jacky", "sha256$AH62hcfABzGv07dX$cc410d510852a5f49bd6bb544c7b0305e1c371c9dc4a494fa5912c089be9137a", "18922251299@163.com");
 
 -- TODO maybe we could using this, but it's a kind of shit
 -- https://github.com/jackyliu16/small-toys/blob/main/Convert_xlsx_Into_SQL_Insert_Query/generate_sql_from_csv.py
 -- source ./SQL/data/insertData.sql;
+
+CREATE TABLE feedback (
+    id        INT PRIMARY KEY AUTO_INCREMENT,
+    user_id      INT DEFAULT 0,    -- for others ref to a user
+    comment  VARCHAR(500) NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES customer(id)
+);
+INSERT INTO feedback VALUES(1, 1, "what a good website");
