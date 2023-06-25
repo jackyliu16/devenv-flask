ac4bbe431c83458b30c77a2d04a64c19d9527d90
jackyliu16 <18922251299@163.com>
2023-06-25 10:37:47 +0800

    db(feedback): add item of email

--- SQL/travel.sql
+++ SQL/travel.sql
@@ -28,7 +28,8 @@ INSERT INTO admin VALUES (1, "jacky", "sha256$AH62hcfABzGv07dX$cc410d510852a5f49
 CREATE TABLE feedback (
     id        INT PRIMARY KEY AUTO_INCREMENT,
     user_id      INT DEFAULT 0,    -- for others ref to a user
+    email    VARCHAR(36)  NOT NULL,
     comment  VARCHAR(500) NOT NULL,
     FOREIGN KEY (user_id) REFERENCES customer(id)
 );
-INSERT INTO feedback VALUES(1, 1, "what a good website");
+INSERT INTO feedback VALUES(1, 1, "18922251299@163.com", "what a good website");
